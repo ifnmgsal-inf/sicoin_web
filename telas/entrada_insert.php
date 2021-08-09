4df66f356c09c0504123362ca13dfc9a7663f476
RToramaru <rafael.almeida.soares2012@gmail.com>
2021-08-09 10:41:23 -0300

    correcoes

--- telas/entrada_insert.php
+++ telas/entrada_insert.php
@@ -35,36 +35,36 @@ foreach($dados_array as $valor){
 $sql = "INSERT INTO entrada (codigo, origem, destino, emissao, recebimento)
 VALUES ('$codigo','$origem','$destino','$emissao', '$recebimento')";
 if (mysqli_query($conn, $sql)) {
-  ?>
+    ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta charset="UTF-8" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <meta charset='UTF-8' />
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
 </head>
 
 <body>
 
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Sucesso</h4>
+    <div class='modal fade' id='myModal' tabindex='-1' role='dialog'>
+        <div class='modal-dialog'>
+            <div class='modal-content'>
+                <div class="modal-header bg-success text-white">
+                    <h4 class='modal-title'>Sucesso</h4>
                 </div>
-                <div class="modal-body">
-                    Cadastrado com sucesso!
+                <div class='modal-body'>
+                    <h4>Cadastrado com sucesso!</h4>
                 </div>
-                <div class="modal-footer">
-                    <input class="btn btn-primary" data-dismiss="modal" value="Fechar" id="modal" onclick='confirmar()'>
+                <div class='modal-footer'>
+                    <input class='btn btn-success' data-dismiss='modal' value='Fechar' id='modal' onclick='confirmar()'>
                 </div>
             </div>
         </div>
     </div>
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>
+    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
 
     <script>
     $(document).ready(function() {
@@ -72,13 +72,54 @@ if (mysqli_query($conn, $sql)) {
     })
 
     function confirmar() {
-        window.location.replace("entrada.php")
+        window.location.replace('entrada.php')
     }
     </script>
 </body>
 <?php
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+          } else {
+              ?>
+<!DOCTYPE html>
+<html>
 
+<head>
+    <meta charset='UTF-8' />
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+</head>
+
+<body>
+
+    <div class='modal fade' id='myModal' tabindex='-1' role='dialog'>
+        <div class='modal-dialog'>
+            <div class='modal-content'>
+                <div class="modal-header bg-danger text-white">
+                    <h4 class='modal-title'>Erro</h4>
+                </div>
+                <div class='modal-body'>
+                    <h4>Ocorreu um erro ao cadastrar!</h4>
+                </div>
+                <div class='modal-footer'>
+                    <input class='btn btn-danger' data-dismiss='modal' value='Fechar' id='modal' onclick='confirmar()'>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>
+    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+
+    <script>
+    $(document).ready(function() {
+        $('#myModal').modal('show')
+    })
+
+    function confirmar() {
+        window.location.replace('entrada.php')
+    }
+    </script>
+</body>
+<?php
+}
+          
 ?>
--- telas/relatorio.php
+++ telas/relatorio.php
@@ -141,6 +141,10 @@ if(!$_SESSION['tipo']){
             </div>
         </footer>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
